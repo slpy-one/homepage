2c2256e5ceeaf4b82ca327d1c3be1e1a38dfc0f3
Suphakit P <me@suphakit.net>
2023-09-11 03:49:45 +0700

    feat<components>: added components

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+
+  const Button = "p-4 hover:bg-ctp-overlay0 hover:text-ctp-base hover:opacity-60"
+
+  const ActiveButton = `${Button}`
+</script>
+
+<div class="w-full fixed top-0 flex flex-row z-30">
+  <div class="w-full justify-start text-left py-4">
+    {#if $page.url.pathname == '/'}
+      <!-- svelte-ignore a11y-invalid-attribute -->
+      <a href="#" class={ActiveButton}>
+        slpy.one
+      </a>
+    {:else}
+      <a href="/" class={Button}>
+        slpy.one
+      </a>
+    {/if}
+  </div>
+  <div class="w-full justify-end text-right py-4">
+    {#if $page.url.pathname == '/about'}
+      <!-- svelte-ignore a11y-invalid-attribute -->
+      <a href="#" class={ActiveButton}>
+        About
+      </a>
+    {:else}
+      <a href="/about" class={Button}>
+        About
+      </a>
+    {/if}
+  </div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script>
 	import './main.css';
-  import Footer from '../lib/components/footer.svelte';
+
+	import Navbar from '../lib/components/Navbar.svelte';
+  import Footer from '../lib/components/Footer.svelte';
 </script>
 
 <div class="min-h-screen w-full bg-ctp-base text-ctp-text">
+	<Navbar />
 	<main>
 		<slot />
 	</main>
