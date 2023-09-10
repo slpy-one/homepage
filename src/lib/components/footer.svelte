9748ff4b3bbd490f0b1b0e646ae909826097d2a2
Suphakit P <me@suphakit.net>
2023-09-11 03:26:11 +0700

    feat<components>: added footer

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import TW from '../constant/tailwind'
+</script>
+
+<div class="w-full text-center fixed bottom-0 py-4">
+  &copy; 2023 <a href="https://github.com/slpy-one" target="_blank" class={`${TW.link}`}>slpy one team</a> All right reserved.
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import './main.css';
+  import Footer from '../lib/components/footer.svelte';
+</script>
+
+<div class="min-h-screen w-full bg-ctp-base text-ctp-text">
+	<main>
+		<slot />
+	</main>
+  <Footer />
+</div>
+
+<style>
+	
+</style>
