d417291fb49d656f176c29704df187eb963846a2
Suphakit P <me@suphakit.net>
2023-09-11 03:50:38 +0700

    feat<page>: added homepage

---
+++ src/lib/db/services.ts
@@ -0,0 +1,8 @@
+const slpyService = [
+	{
+		name: 'SLPY Timestamp',
+		url: 'https://timestamp.slpy.one'
+	}
+];
+
+export default slpyService;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import TW from "../lib/constant/tailwind";
+  import slpyService from "../lib/db/services";
+</script>
+
+<section class="md:w-[80%] w-full min-h-screen flex flex-row items-center justify-center text-center mx-auto">
+  <div class="h-[70vh] w-full overflow-y-auto">
+    <h1 class={`${TW.heading.xxxl}`}>
+      slpy one | Services
+    </h1>
+    <div class="w-[80%] flex flex-row flex-wrap justify-center text-center gap-4 mx-auto">
+      {#each slpyService as service}
+        <a class="block w-[100%] md:w-[40%] h-auto py-4 border-2 text-lg rounded hover:opacity-60" href={service.url} target="_blank">
+          {service.name}
+        </a>
+      {/each}
+    </div>
+  </div>
+</section>
