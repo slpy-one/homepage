077ea432b78fdd8cf2d38ad78fedc67fa5f5d437
Suphakit P. <me@suphakit.net>
2025-02-11 16:09:15 +0700

    feat<db>: update db

--- src/lib/db/services.ts
+++ src/lib/db/services.ts
@@ -2,6 +2,10 @@ const slpyService = [
 	{
 		name: 'SLPY Timestamp',
 		url: 'https://timestamp.slpy.one'
+	},
+	{
+		name: 'Now Playing',
+		url: 'https://now.slpy.one'
 	}
 ];
 
--- src/lib/db/team.ts
+++ src/lib/db/team.ts
@@ -4,16 +4,16 @@ const teams = [
 		username: 'dethMastery',
 		pics: 'https://avatars.githubusercontent.com/u/25049577?v=4'
 	},
-	{
-		name: 'Khanaphon "kana" Phaengtan',
-		username: 'kana2011th',
-		pics: 'https://avatars.githubusercontent.com/u/1833001?v=4'
-	},
-	{
-		name: 'Harmoナイア',
-		username: 'LazyHarmo',
-		pics: 'https://avatars.githubusercontent.com/u/105147522?v=4'
-	},
+	// {
+	// 	name: 'Khanaphon "kana" Phaengtan',
+	// 	username: 'kana2011th',
+	// 	pics: 'https://avatars.githubusercontent.com/u/1833001?v=4'
+	// },
+	// {
+	// 	name: 'Harmoナイア',
+	// 	username: 'LazyHarmo',
+	// 	pics: 'https://avatars.githubusercontent.com/u/105147522?v=4'
+	// },
 	{
 		name: 'xtsusaku',
 		username: 'xtsusaku',
