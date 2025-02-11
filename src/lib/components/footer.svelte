069814cae520be467e5695b3ccac88ebfc2e5c1d
Suphakit P. <me@suphakit.net>
2025-02-11 16:09:33 +0700

    feat<component>: update copyright year

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -3,5 +3,5 @@
 </script>
 
 <div class="w-full text-center fixed bottom-0 py-4">
-  &copy; 2023 <a href="https://github.com/slpy-one" target="_blank" class={`${TW.link}`}>slpy one team</a> All right reserved.
+  &copy; 2025 <a href="https://github.com/slpy-one" target="_blank" class={`${TW.link}`}>slpy one team</a> All right reserved.
 </div>
